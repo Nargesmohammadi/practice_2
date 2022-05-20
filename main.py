b849829b72f83d5d387d7a4c4c75e9220cb63a01
mitraa <mitraa@pop-os.localdomain>
2022-05-20 22:48:24 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+def newLine():
+    print()
+
+
+def threeLines():
+    newLine()
+    newLine()
+    newLine()
+
+
+print("First Line.")
+threeLines()
+print("Second Line")
+
+
+def twentynineLines():
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+    threeLines()
+
+
+print(" Hello")
+twentynineLines()
+print("Welcome")
+
+
+def nineLines():
+    threeLines()
+    threeLines()
+    threeLines()
+
+
+nineLines()
+print("Let's go")
+
+
+def fiveLines():
+    newLine()
+    newLine()
+    newLine()
+    newLine()
+    newLine()
+
+
+fiveLines()
+print("******")
